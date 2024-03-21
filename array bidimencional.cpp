4e723832f7470e8ddd9b7f527d6ead2aa0c77439
Your Name <you@example.com>
2024-03-20 21:31:56 -0600

    tarea numero 4

---
+++ array bidimencional.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+main(){
+
+
+ 	//Arreglos bidimensionales, matriz, tabla
+ 	int fil=0,col=0;
+ 	cout<<"Ingrese Filas:";
+ 	cin>>fil;
+ 	cout<<"Ingrese Columnas:";
+	 cin>>col;
+ 	int matriz[fil][col];
+	 cout<<" ---------- Ingresar ----------"<<endl;
+ 	for (int i=0;i<fil;i++){
+ 		for(int ii=0;ii<col;ii++){
+ 			cout<<i<<","<<ii<<": ";
+ 			cin>>matriz[i][ii];
+		 }
+	 }
+ 	cout<<" ---------- Mostrar ----------"<<endl;
+ 	for (int i=0;i<fil;i++){
+ 		for(int ii=0;ii<col;ii++){
+ 			cout<<i<<","<<ii<<": "<<matriz[i][ii]<<endl;
+		}
+ 	}
+}
